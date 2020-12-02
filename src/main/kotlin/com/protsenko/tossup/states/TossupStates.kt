654de6b5c548ad544e5df2f3d14272ef698b67ae
Mykyta Protsenko <mykyta@protsenko.com>
2020-12-01 21:08:37 -0800

    Implemented Tossup States

---
+++ src/main/kotlin/com/protsenko/tossup/states/TossupStates.kt
@@ -0,0 +1,23 @@
+package com.protsenko.tossup.states
+
+class TossupStates : States {
+    override fun states(): List<State> {
+        return listOf(
+            State("Wisconsin", 10),
+            State("Michigan", 16),
+            State("Minnesota", 10),
+            State("Iowa", 6),
+            State("North Carolina", 15),
+            State("Arizona", 11),
+            State("Florida", 29),
+            State("Nevada", 6),
+            State("Georgia", 16),
+            State("Texas", 38),
+            State("Pennsylvania", 20),
+            State("Maine CD2", 1),
+            State("Ohio", 18),
+            State("Nebraska CD2", 1),
+        )
+    }
+
+}
