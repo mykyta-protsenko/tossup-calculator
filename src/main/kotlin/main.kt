66a3ebe018b224ef96dd95e2900641421efdc5a7
Mykyta Protsenko <mykyta@protsenko.com>
2020-12-01 22:02:42 -0800

    Implemented OutcomeCalculator

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import com.protsenko.tossup.calculator.OutcomeCalculator
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("The probability of Biden's victory is " + OutcomeCalculator().chanceOfBidenVictory())
 }
